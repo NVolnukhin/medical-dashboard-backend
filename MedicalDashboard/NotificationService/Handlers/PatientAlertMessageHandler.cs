7b162ed33e5fa194eede29389596b5490888fd0c
NVolnukhin <144201108+NVolnukhin@users.noreply.github.com>
2025-07-04 00:29:16 +0300

    alert hadler

---
+++ MedicalDashboard/NotificationService/Handlers/PatientAlertMessageHandler.cs
@@ -0,0 +1,44 @@
+using System.Text.Encodings.Web;
+using System.Text.Json;
+using NotificationService.Data.Models;
+using NotificationService.Enums;
+using NotificationService.Services.Notification;
+using NotificationService.Extensions.Logging;
+
+namespace NotificationService.Handlers;
+
+public class PatientAlertMessageHandler : IMessageHandler<PatientAlertMessage>
+{
+    private readonly INotificationService _notificationService;
+    private readonly ILogger<PatientAlertMessageHandler> _logger;
+
+    public string Topic => "md-alerts";
+
+    public PatientAlertMessageHandler(INotificationService notificationService, ILogger<PatientAlertMessageHandler> logger)
+    {
+        _notificationService = notificationService;
+        _logger = logger;
+    }
+
+    public async Task HandleAsync(PatientAlertMessage message, CancellationToken cancellationToken)
+    {
+        _logger.LogInfo($"Обработка сообщения для пациента: {message.PatientName} ({message.PatientId})");
+
+        var options = new JsonSerializerOptions
+        {
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+        };
+        
+        var messageBody = JsonSerializer.Serialize(message, options);
+
+        var notificationRequest = new NotificationRequest
+        {
+            Recipient = message.PatientId,
+            Subject = $"Оповещение: {message.AlertType}",
+            Body = messageBody,
+            Type = NotificationType.WebPush
+        };
+
+        await _notificationService.SendNotificationAsync(notificationRequest, cancellationToken);
+    }
+}
