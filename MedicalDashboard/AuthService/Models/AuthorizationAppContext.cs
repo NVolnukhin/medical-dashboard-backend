22934967a29cf4a72b17210b931fa7d863cb30c4
NVolnukhin <144201108+NVolnukhin@users.noreply.github.com>
2025-07-04 18:47:37 +0300

    db context

---
+++ MedicalDashboard/AuthService/Models/AuthorizationAppContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AuthService.Models
+{
+    public class AuthorizationAppContext(DbContextOptions<AuthorizationAppContext> options)
+        : DbContext(options)
+    {
+        public DbSet<User> Users => Set<User>();
+        public DbSet<PasswordRecoveryToken> PasswordRecoveryTokens => Set<PasswordRecoveryToken>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Уникальные ограничения
+            modelBuilder.Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+        }
+    }
+}
