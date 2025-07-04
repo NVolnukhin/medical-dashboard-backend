deb918b096ae92c9bbc5f72e532a6f13f2f55a78
NVolnukhin <144201108+NVolnukhin@users.noreply.github.com>
2025-07-04 18:48:28 +0300

    user repo

---
+++ MedicalDashboard/AuthService/Repository/User/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AuthService.Repository.User
+{
+    public interface IUserRepository
+    {
+        public DbSet<AuthService.Models.User> Users { get; }
+
+        Task SaveChangesAsync();
+        
+        public Task<AuthService.Models.User?> GetById(Guid userId);
+        public  Task<AuthService.Models.User?> GetByEmail(string email);
+        public Task UpdatePassword(Guid userId, string newPasswordHash, string newSalt);
+    }
+}
