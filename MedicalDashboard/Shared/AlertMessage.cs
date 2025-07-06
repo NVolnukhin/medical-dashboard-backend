849e6faaa6974b773d2e94b06ec8e43d8a02a201
nikita.volnukhin <heyshehe@gmail.com>
2025-07-06 13:21:54 +0300

    убрал дублирующую модель

--- MedicalDashboard/Shared/AlertMessage.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Shared;
-
-public class AlertMessage
-{
-    public Guid PatientId { get; set; }
-    public string PatientName { get; set; } = string.Empty;
-    public string AlertType { get; set; } = string.Empty; // "alert" или "warning"
-    public string MetricType { get; set; } = string.Empty; // например "pulse"
-} 
