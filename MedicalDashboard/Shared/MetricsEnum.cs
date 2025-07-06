37758985fb56b2d35d2a5cee75ecc5efcfa0f08c
nikita.volnukhin <heyshehe@gmail.com>
2025-07-06 16:49:05 +0300

    енамчик для метрик

---
+++ MedicalDashboard/Shared/MetricsEnum.cs
@@ -0,0 +1,14 @@
+namespace Shared;
+
+public enum MetricType
+{
+    Pulse,                   // пульс
+    RespirationRate,         // частота дыхания
+    Temperature,             // температура
+    SystolicPressure,        // давление верхнее
+    DiastolicPressure,       // давление нижнее
+    Saturation,              // сатурация
+    Weight,                  // вес
+    Hemoglobin,              // гемоглобин
+    Cholesterol              // холестерин
+}
