4633fb95f4e0bb30ac1ade60085c364b5048490c
nikita.volnukhin <heyshehe@gmail.com>
2025-07-06 17:48:02 +0300

    ДТОшки

---
+++ MedicalDashboard/DataAnalysisService/DTOs/KafkaTopics.cs
@@ -0,0 +1,7 @@
+﻿namespace DataAnalysisService.DTOs;
+
+public class KafkaTopics
+{
+    public string RawMetrics { get; set; } = string.Empty;
+    public string Alerts { get; set; } = string.Empty;
+} 
